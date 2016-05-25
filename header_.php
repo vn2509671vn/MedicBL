d5fbba643e73a4f4dd190f1fb99889aa5d944864
vn2509671vn <vn2509671vn@gmail.com>
2016-05-25 17:42:41 +0700

    re-add header.php

---
+++ header_.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Medic Bạc Liêu</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.js"></script>
+  <!-- include summernote css/js-->
+  <link href="editorPlugin/summernote.css" rel="stylesheet">
+  <script src="editorPlugin/summernote.js"></script>
+</head>
+<body>
+      <div class="container">
+        <div class="col-md-6 col-xs-4">
+          <img src="images/logo/bl-logo-transparentsm.png" width="auto" height="80">
+        </div>
+        <div class="col-md-6 col-xs-8">
+          <form class="navbar-form navbar-right" role="search">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Tìm kiếm...">
+                    <span class="input-group-btn">
+                        <button type="submit" class="btn btn-default">
+                        <span class="glyphicon glyphicon-search"></span>
+                        </button>
+                    </span>
+                </div>
+          </form>
+
+        </div>
+      </div>
+ 
+      
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-default navbar-nav">
+                <li><a href="content.php">Trang Chủ</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" >Giới Thiệu <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Giới thiệu chung</a></li>
+                    <li><a href="#">Ban lãnh đạo</a></li>
+                    <li><a href="#">Đội ngũ bác sĩ</a></li>
+                  </ul>
+                </li>
+                <li><a href="_TestEditor.php">Test Editor</a></li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
