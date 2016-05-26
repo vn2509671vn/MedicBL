d0f29d6358bb52a54dc0278a5678affcf7c2c6e0
Tran Giang Minh Thang <vn2509671vn@gmail.com>
2016-05-26 09:42:05 +0000

    Commit code

--- content.php
+++ content.php
@@ -13,30 +13,30 @@
       <div class="col-sm-12">
         <h3 class="text-left text-green">Tin tức</h3>
         <hr class="divider">
-        <div class="row">
-          <div id="article_img" class="col-md-3">
-            <img src="http://www.hoanmy.com/Data/Sites/2/userfiles/20/HMCL/CLB%20%C4%90T%C4%90/Poster%2040%20x%2060_40%20x%2060.jpg" width="100%"></img>
+        <div class="row text-left">
+          <div class="col-sm-4"><a href="#" class=""><img src="http://placehold.it/1280X720" class="img-responsive"></a>
           </div>
-          <div id="article_content" class="col-md-9">
-            <div class="row text-left" id="article_title">
-              <h3>Title 1</h3>
-            </div>
-            <div class="row text-left" id="article_date">
-              <?php 
-              $dateTime = getdate();
-              echo "$dateTime[weekday], $dateTime[month] $dateTime[mday], $dateTime[year]";
-              ?>
-            </div>
-            <div class="row text-left" id="article_main_content">
-              <p>Cố gắng thoát khỏi những đêm dài vắng em
-                  Chỉ mình anh với chiếc bóng trên tường
-                  Lòng anh day dứt không yên chỉ cố gắng không ngã quỵ
-              </p>
-            </div>
+          <div class="col-sm-8">
+            <h3 class="title">How to Fight Fraud with Artificial Intelligence and Intelligent Analytics</h3>
+            <p class="text-muted"><span class="glyphicon glyphicon-calendar"></span> July 23, 2014 @ 1:30 PM</p>
+            <p>Could artificial intelligence have been used to prevent the high-profile Target breach? The concept is not so far-fetched. Organizations such as Mastercard and RBS WorldPay have long relied on artificial intelligence to detect fraudulent transaction patterns and prevent card.</p>
+            <p class="text-muted">Presented by <a href="#">ThangTGM</a></p>
           </div>
         </div>
+        <hr class="divider">
+        <div class="row text-left">
+          <div class="col-sm-4"><a href="#" class=""><img src="http://placehold.it/1280X720" class="img-responsive"></a>
+          </div>
+          <div class="col-sm-8">
+            <h3 class="title">Big Payment Data: Leveraging Transactional Data to Ensure an Enterprise Approach to Risk Management</h3>
+            <p class="text-muted"><span class="glyphicon glyphicon-calendar"></span> July 23, 2014 @ 1:30 PM</p>
+            <p>60% of organizations were exposed to actual or attempted fraud loss last year. As fraud and risk increases year over year, the amount of data being collected increases as well.</p>
+            <p class="text-muted">Presented by <a href="#">ThangTGM</a></p>
+          </div>
+        </div>
+        <hr class="divider">
+        </div>
       </div>
-    </div>
 
     <!-- Right content-->
     <div class="col-sm-4">
@@ -49,10 +49,13 @@
       </div>
       <div class="row">
           <div class="col-sm-12">
-            <iframe width="100%" height="auto" src="http://www.youtube.com/embed/KgMt0dtr4Vc" frameborder="0" allowfullscreen></iframe>
+            <iframe width="100%" height="auto" src="https://www.youtube.com/embed/KgMt0dtr4Vc" frameborder="0" allowfullscreen></iframe>
           </div>
-        </div>
       </div>
+      <div class="row">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3937.5422161982165!2d105.71858151522143!3d9.28513048748028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a109637bdecd2b%3A0x8efb538ee8adb88e!2zQuG7h25oIFZp4buHbiDEkGEgS2hvYSBUaGFuaCBWxak!5e0!3m2!1svi!2s!4v1464248210435" width="100%" height="auto" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+    </div>
   </div>
 </div>
 <!-- Add start footer-->
