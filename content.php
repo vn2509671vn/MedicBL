df53304fc7ed77a490c53daef058a67a28719a97
Tran Giang Minh Thang <vn2509671vn@gmail.com>
2016-05-26 02:36:06 +0000

    Test commit

--- content.php
+++ content.php
@@ -14,8 +14,26 @@
         <h3 class="text-left text-green">Tin tức</h3>
         <hr class="divider">
         <div class="row">
-          <div id="article_img" class="col-md-3">abc</div>
-          <div id="article_content" class="col-md-9">xyz</div>
+          <div id="article_img" class="col-md-3">
+            <img src="http://www.hoanmy.com/Data/Sites/2/userfiles/20/HMCL/CLB%20%C4%90T%C4%90/Poster%2040%20x%2060_40%20x%2060.jpg" width="100%"></img>
+          </div>
+          <div id="article_content" class="col-md-9">
+            <div class="row text-left" id="article_title">
+              <h3>Title 1</h3>
+            </div>
+            <div class="row text-left" id="article_date">
+              <?php 
+              $dateTime = getdate();
+              echo "$dateTime[weekday], $dateTime[month] $dateTime[mday], $dateTime[year]";
+              ?>
+            </div>
+            <div class="row text-left" id="article_main_content">
+              <p>Cố gắng thoát khỏi những đêm dài vắng em
+                  Chỉ mình anh với chiếc bóng trên tường
+                  Lòng anh day dứt không yên chỉ cố gắng không ngã quỵ
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
