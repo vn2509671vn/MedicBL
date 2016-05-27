e5bbcfec0cc218a7856656c6d1fb20deac9a72ae
Tran Giang Minh Thang <vn2509671vn@gmail.com>
2016-05-27 09:33:39 +0000

    Add info hospital to footer

--- footer.php
+++ footer.php
@@ -1,10 +1,21 @@
 <a href="#" class="scrollup">Go Top</a>
 <footer class="container-fluid text-center">
-    <div class="text-center center-block">
+   <div class="container text-left">
+       <div class="col-md-12">
+           <p><h3>Medic Bạc Liêu</h3></p>
+       </div>
+       <div class="col-md-4">
+        <p>Address:
+        <br>
+        Phone:
+        </p>
+       </div>
+       <div class="col-md-4">
         <a class="btn btn-social-icon btn-facebook"><span class="fa fa-facebook"></span></a>
         <a class="btn btn-social-icon btn-google"><span class="fa fa-google"></span></a>
         <a class="btn btn-social-icon btn-youtube"><span class="fa fa-youtube"></span></a>
-    </div>
+       </div>
+   </div>
 </footer>
 </body>
 </html>
